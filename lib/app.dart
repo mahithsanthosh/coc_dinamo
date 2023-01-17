04d6c7ba3a955cea6c99ecc95d4bd500511cc161
anlinQB <anlin@qburst.com>
2023-01-17 14:06:24 +0530

    webhook test

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'dinamo',
+      title: 'dinam',
       home: const SearchScreen(),
     );
   }
